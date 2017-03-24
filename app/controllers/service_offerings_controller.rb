91f5a7c9764e6b3e87f3800a0269b9cf830a0714
frankwarren21 <frank.warren@gmail.com>
2017-03-24 14:00:00 -0400

    added services offered buttons and list to agnecy show page

--- app/controllers/service_offerings_controller.rb
+++ app/controllers/service_offerings_controller.rb
@@ -69,6 +69,6 @@ class ServiceOfferingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def service_offering_params
-      params.require(:service_offering).permit(:name)
+      params.require(:service_offering).permit(:name, :agency)
     end
 end
